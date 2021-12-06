7ba9393ac927174b62adf18e0ef7cda713cd3d4a
unknown <ballatoremanon@gmail.com>
2021-12-06 14:41:23 +0100

    ajout des fonctionnalité luminositer contraster et cooordonnée

--- coord.js
+++ coord.js
@@ -5,22 +5,23 @@ function coordonnée() {
 	let mark = document.createElement("img");
 	mark.src = "marker.jpg";
 	mark.id = "marker";
+	mark.style.display = "none";
 	document.body.appendChild(mark);
 	let checkbox = document
 		.getElementById("checkbox_coord")
 		.addEventListener("change", (e) => {
 			this.checkboxValue = e.target.checked ? "on" : "off";
-			if (checkboxValue == "on") {
+			if (checkboxValue == "off") {
+				with (document.getElementById("marker")) {
+					style.display = "none";
+				}
+			} else if (checkboxValue == "on") {
 				with (document.getElementById("marker")) {
 					style.left = Y;
 					style.top = X;
 					style.display = "block";
 					style.zIndex = "1";
 					style.position = "absolute";
-				}
-			} else if (checkboxValue == "off") {
-				with (document.getElementById("marker")) {
-					style.display = "none";
 				}
 			}
 		});
--- lumiContrast.js
+++ lumiContrast.js
@@ -1,8 +1,8 @@
 function brightness(val) {
-	document.getElementById("img1").style.filter = `brightness(${val})`;
+	document.getElementById("img").style.filter = `brightness(${val})`;
 	let af = (document.getElementById("lumi_affi").innerHTML = val);
 }
 function contrast(val) {
-	document.getElementById("img1").style.filter = `contrast(${val})`;
+	document.getElementById("img").style.filter = `contrast(${val})`;
 	let af = (document.getElementById("con_affi").innerHTML = val);
 }
