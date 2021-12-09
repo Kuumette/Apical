b68f91746a00b3bcc416b781198c0663970c1526
Kuumette <ballatoremanon@gmail.com>
2021-12-09 09:35:52 +0100

    menu deroulant

--- fullScreen.js
+++ fullScreen.js
@@ -12,15 +12,31 @@ document.addEventListener(
 	false
 );
 let disa = document.getElementById("d2");
-
+let lastImage = document.getElementById("lastImage");
+let lastSubstractionImage = document.getElementById("lastSubstractionImage");
+let lastAnimation = document.getElementById("lastAnimation");
+let lastSubstractionAnimation = document.getElementById(
+	"lastSubstractionAnimation"
+);
+let reglage = document.querySelector(".reglage");
 function toggleFullScreen() {
 	if (!document.fullscreenElement) {
 		document.documentElement.requestFullscreen();
 		disa.style.display = "none";
+		lastImage.style.display = "none";
+		lastSubstractionImage.style.display = "none";
+		lastAnimation.style.display = "none";
+		lastSubstractionAnimation.style.display = "none";
+		reglage.style.display = "none";
 	} else {
 		if (document.exitFullscreen) {
 			document.exitFullscreen();
 			disa.style.display = "block";
+			lastImage.style.display = "block";
+			lastSubstractionImage.style.display = "block";
+			lastAnimation.style.display = "block";
+			lastSubstractionAnimation.style.display = "block";
+			reglage.style.display = "block";
 		}
 	}
 }
