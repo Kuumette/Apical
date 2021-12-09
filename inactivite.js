50cdd58e792ded8ce4d125c161cfdffa219d33e9
unknown <ballatoremanon@gmail.com>
2021-12-09 15:53:54 +0100

    modif inactivité

--- inactivite.js
+++ inactivite.js
@@ -9,7 +9,7 @@ function resetTemps() {
 	document.querySelector(".delai").style.display = "none";
 	clearInterval(temps);
 	secondes = 0;
-	temps = setInterval(startTemps, 600000);
+	temps = setInterval(startTemps, 1000);
 	display.style.display = "block";
 	overlay.style.display = "block";
 	display1.style.display = "block";
@@ -25,7 +25,7 @@ window.onmousedown = resetTemps;
 function startTemps() {
 	secondes++;
 	document.querySelector(".secondes").textContent = secondes;
-	document.querySelector(".delai").style.display = "block";
+	document.querySelector(".delai").style.display = "none";
 	display.style.display = "none";
 	overlay.style.display = "none";
 	display1.style.display = "none";
