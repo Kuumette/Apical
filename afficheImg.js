d930da53e8b1fdc876c78123a49cac558ee63744
unknown <ballatoremanon@gmail.com>
2021-12-13 15:22:55 +0100

    un peu de style

--- afficheImg.js
+++ afficheImg.js
@@ -85,8 +85,8 @@ request.onload = function () {
 		video.setAttribute("controls", " ");
 		video.setAttribute("preload", "auto");
 		video.setAttribute("data-setup", "{}");
-		video.setAttribute("width", "900");
-		video.setAttribute("height", "520");
+		video.setAttribute("width", "1000");
+		video.setAttribute("height", "620");
 
 		var source = document.createElement("source");
 		source.setAttribute("src", v.lastAnimation.img);
@@ -107,8 +107,8 @@ request.onload = function () {
 		video1.setAttribute("controls", " ");
 		video1.setAttribute("preload", "auto");
 		video1.setAttribute("data-setup", "{}");
-		video1.setAttribute("width", "900");
-		video1.setAttribute("height", "520");
+		video1.setAttribute("width", "1000");
+		video1.setAttribute("height", "620");
 
 		var sourcea = document.createElement("source");
 		sourcea.setAttribute("src", v.lastSubstractionAnimation.img);
--- inactivite.js
+++ inactivite.js
@@ -4,15 +4,17 @@ let temps,
 let display = document.getElementById("reglage");
 let overlay = document.getElementById("last");
 let display1 = document.getElementById("nav");
+let display2 = document.getElementById("auto");
 
 function resetTemps() {
 	document.querySelector(".delai").style.display = "none";
 	clearInterval(temps);
 	secondes = 0;
-	temps = setInterval(startTemps, 6000000);
+	temps = setInterval(startTemps, 10000);
 	display.style.opacity = "1";
 	overlay.style.opacity = "1";
 	display1.style.opacity = "1";
+	display2.style.opacity = "1";
 }
 
 window.onload = resetTemps;
@@ -29,4 +31,5 @@ function startTemps() {
 	display.style.opacity = "0";
 	overlay.style.opacity = "0";
 	display1.style.opacity = "0";
+	display2.style.opacity = "0";
 }
