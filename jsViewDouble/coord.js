5d0fff050c5fbd3a7e556d5df7bb91084a6d913f
Kuumette <ballatoremanon@gmail.com>
2021-12-21 09:18:20 +0100

    media query

---
+++ jsViewDouble/coord.js
@@ -0,0 +1,29 @@
+var reqURL = "../config/pointCoord_overlay.json";
+var req = new XMLHttpRequest();
+req.open("GET", reqURL);
+req.responseType = "json";
+req.send();
+
+req.onload = function () {
+	let overlay = req.response;
+	let parent = document.querySelector("#gauche");
+	let UT1 = document.createElement("img");
+	UT1.src = overlay.img;
+	UT1.id = "overlay";
+	parent.appendChild(UT1);
+	UT1.style.display = "none";
+	document
+		.getElementById("checkbox_coord")
+		.addEventListener("change", (e) => {
+			checkboxValue = e.target.checked ? "on" : "off";
+
+			if (checkboxValue == "off") {
+				console.log(checkboxValue);
+
+				UT1.style.display = "none";
+			} else if (checkboxValue == "on") {
+				console.log(checkboxValue);
+				UT1.style.display = "block";
+			}
+		});
+};
