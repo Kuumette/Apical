2f6cf69407b59dc90b6d8cd915f4d706d71e813b
unknown <ballatoremanon@gmail.com>
2022-02-07 11:33:38 +0100

    reglage inactivité

--- jsViewSample/coord.js
+++ jsViewSample/coord.js
@@ -1,4 +1,5 @@
-var reqURL = "../config/pointCoord_overlay.json";
+var reqURL =
+	"https://live.neos360.com/eso/paranal/apicam/config/pointCoord_overlay.json";
 var req = new XMLHttpRequest();
 req.open("GET", reqURL);
 req.responseType = "json";
