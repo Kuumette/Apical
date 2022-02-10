7d48a2afc9a28f921c8a2a60b15a1d71a63c66ab
Kuumette <ballatoremanon@gmail.com>
2022-02-10 13:35:16 +0100

    refresh ok et tester

--- jsViewDouble/afficheImgDroite.js
+++ jsViewDouble/afficheImgDroite.js
@@ -1,4 +1,4 @@
-let reURL = "https://live.neos360.com/eso/paranal/apicam/config/image.json";
+let reURL = "https://live.neos360.com/eso/paranal/apicam/image.json";
 let re = new XMLHttpRequest();
 re.open("GET", reURL);
 re.responseType = "json";
