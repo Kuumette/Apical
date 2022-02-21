94da1e14d035489cb84acaa177400d599644d947
unknown <ballatoremanon@gmail.com>
2022-02-21 15:09:16 +0100

    ...

--- assets/js/Components/loadView.js
+++ assets/js/Components/loadView.js
@@ -1,46 +1,55 @@
 /** La fonction de reload on peux la mettre ailleurs 😁 */
 function loadView(type, side, displayImage, brightness, contrast) {
-  /**
-   * Pour accerder à une information de notre json
-   * il faut écrire `displayImage[type]`
-   */
+	/**
+	 * Pour accerder à une information de notre json
+	 * il faut écrire `displayImage[type]`
+	 */
 
-  /** Je récupère les infos du DOM */
-  const CONTENT = document.querySelector(`#${side} #content`);
-  const INFO_IMG = document.querySelector(`#${side} #infoImg`);
+	/** Je récupère les infos du DOM */
+	const CONTENT = document.querySelector(`#${side} #content`);
+	const INFO_IMG = document.querySelector(`#${side} #infoImg`);
+	const TOGGLE_COORDS = document.querySelector(`#last`);
+	console.log(TOGGLE_COORDS);
+	let html = "";
 
-  let html = "";
-
-  // J'insere les information reçu depuis displayImage pour l'insérer dans le DOM
-  const infoImg = `
+	// J'insere les information reçu depuis displayImage pour l'insérer dans le DOM
+	const infoImg = `
   <p id="date">${displayImage[type].date}</p>
   <p id="heure">${displayImage[type].heure}</p>
   <p id="name">apicam</p>
   `;
 
-  /** Si c'est une image alors */
-  if (type === "lastImage" || type === "lastSubstractionImage") {
-    html = `
-    <div id="center-${side}" style="display: ${getItem(
-      `display-${side}`
-    )}"></div>
-    <img src="${
-      displayImage[type].img
-    }" alt="description" id="img" name="img-${side}" style="filter: brightness(${brightness}) contrast(${contrast}) !important;"/>
+	/** Si c'est une image alors */
+	if (
+		type === "lastImage" ||
+		type === "lastSubstractionImage" ||
+		type === "panorama" ||
+		type === "attenuation"
+	) {
+		TOGGLE_COORDS.style.display = "block";
+		html = `
+		<div id="center-${side}" style="display: ${getItem(`display-${side}`)}"></div>
+		<img src="${
+			displayImage[type].img
+		}" alt="description" id="img" name="img-${side}" style="filter: brightness(${brightness}) contrast(${contrast}) !important;"/>
     `;
-    // Les informations sont necessaire que pour les images
-    INFO_IMG.innerHTML = infoImg;
-  } else {
-    /** Sinon c'est une vidéo */
-    html = `
-    <div id="center" style="display: ${getItem(`display-${side}`)}"></div>
+		// Les informations sont necessaire que pour les images
+		INFO_IMG.innerHTML = infoImg;
+	} else if (
+		type === "lastAnimation" ||
+		type === "lastSubstractionAnimation"
+	) {
+		TOGGLE_COORDS.style.display = "none";
+		/** Sinon c'est une vidéo */
+		html = `
+		<div id="center" style="display: ${getItem(`display-${side}`)}"></div>
 
-    <video src="${
-      displayImage[type].img
-    }" autoplay preload controls loop class="video-js"></video>`;
-    // j'efface les informations pour les vidéos
-    INFO_IMG.innerHTML = "";
-  }
+		<video src="${
+			displayImage[type].img
+		}" autoplay preload controls loop class="video-js"></video>`;
+		// j'efface les informations pour les vidéos
+		INFO_IMG.innerHTML = "";
+	}
 
-  CONTENT.innerHTML = html;
+	CONTENT.innerHTML = html;
 }
--- assets/js/reload.js
+++ assets/js/reload.js
@@ -1,9 +1,6 @@
 /**
- * 
  * Je créer une function qui m'affiche chaque x seconde mon dernier élément
- * 
  * setInterval de timer qui m'appel afficheImage.js
- * 
  * AfficheImage.js prend en paramètre le tableau de mes images avec comme clé
  * lastImage / lastSubstractionImage / lastAnimation / lastSubstractionAnimation
  * qui ont tous comme data 
@@ -11,9 +8,7 @@
 	 -	"desc": "last picture",
 	 -	"date": "2020-02-16",
 	 -	"heure": "03:48 UT"
- * 
- * 
- * */
+*/
 
 /**
  *
@@ -21,80 +16,105 @@
  * @param {string} side default value is main
  * @description reload la vue demandé par l'utilisateur
  */
-async function reload(type, side = "main") {
-  console.log(`${type} loaded on ${side}`);
-  /** Je fetch les information dont j'ai besoins */
-  const response = await fetch("./assets/config/config.json");
-  const images = await fetch("./assets/config/image.json");
-  const coords = await fetch("./assets/config/pointCoord_overlay.json");
+async function reload(type, side = "main", configFile) {
+	console.log(`${type} loaded on ${side}`);
+	/** Je fetch les information dont j'ai besoins */
+	const response = await fetch(
+		//"https://live.neos360.com/eso/paranal/apicam/config/config.json"
+		"https://live.neos360.com/eso/paranal/apicam/assets/config/config.json"
+	);
+	const { json, serveur } = await response.json();
 
-  /** Je les transformes en JSON */
-  const { dev, prod } = await response.json();
-  const displayImage = await images.json();
-  const tcs = await coords.json();
+	let urlImg = "";
+	if (serveur.isProd === false) {
+		urlImg = serveur.urlDev + json.images;
+	} else {
+		urlImg = serveur.urlProd + json.images;
+	}
 
-  // Je load la 1er image avec les paramètres par défaut.
-  const view = loadView(
-    getItem(`type-${side}`),
-    side,
-    displayImage,
-    getItem(`brightness-${side}`),
-    getItem(`contrast-${side}`)
-  );
+	let urlCoords = "";
+	if (serveur.isProd === false) {
+		urlCoords = serveur.urlDev + json.coords;
+	} else {
+		urlCoords = serveur.urlProd + json.coords;
+	}
 
-  const viewCoords = loadCoords(tcs.img, side);
+	const images = await fetch(urlImg);
+	const coords = await fetch(urlCoords);
 
-  /**
-   * Ici, en plus de reload mon composant IMAGE
-   * je dois aussi ajouter les filtres
-   */
-  if (type === "lastImage" || type === "lastSubstractionImage") {
-    const IMG = document.querySelectorAll("#img");
-    IMG.forEach((img) => {
-      if (img.name === `img-${side}`) {
-        addFilter(
-          img,
-          getItem(`brightness-${side}`),
-          getItem(`contrast-${side}`)
-        );
-      }
-    });
-  }
+	/** Je les transformes en JSON */
+	const displayImage = await images.json();
+	const tcs = await coords.json();
 
-  /** Get TIMER */
-  const refresh = dev.timer;
-  let refreshEnabled = document.querySelector(`#${side} #checkbox-${side}`);
-  let refreshTime = null;
+	// Je load la 1er image avec les paramètres par défaut.
+	const view = loadView(
+		getItem(`type-${side}`),
+		side,
+		displayImage,
+		getItem(`brightness-${side}`),
+		getItem(`contrast-${side}`)
+	);
 
-  /** Get event if button changed */
-  refreshEnabled.addEventListener("change", function () {
-    if (refreshEnabled.checked) {
-      refreshTime = setInterval(() => {
-        loadView(
-          getItem(`type-${side}`),
-          side,
-          displayImage,
-          getItem(`brightness-${side}`),
-          getItem(`contrast-${side}`)
-        );
-        loadCoords(tcs.img, side);
-      }, refresh);
-    } else {
-      clearInterval(refreshTime);
-    }
-  });
+	const viewCoords = loadCoords(tcs.img, side);
+
+	/**
+	 * Ici, en plus de reload mon composant IMAGE
+	 * je dois aussi ajouter les filtres
+	 */
+	if (
+		type === "lastImage" ||
+		type === "lastSubstractionImage" ||
+		type === "panorama" ||
+		type === "attenuation"
+	) {
+		const IMG = document.querySelectorAll("#img");
+		IMG.forEach((img) => {
+			if (img.name === `img-${side}`) {
+				addFilter(
+					img,
+					getItem(`brightness-${side}`),
+					getItem(`contrast-${side}`)
+				);
+			}
+		});
+	}
+
+	/** Get TIMER */
+	const refresh = json.timer;
+	let refreshEnabled = document.querySelector(`#${side} #checkbox-${side}`);
+	let refreshTime = null;
+
+	/** Get event if button changed */
+	refreshEnabled.addEventListener("change", function () {
+		if (refreshEnabled.checked) {
+			refreshTime = setInterval(() => {
+				loadView(
+					getItem(`type-${side}`),
+					side,
+					displayImage,
+					getItem(`brightness-${side}`),
+					getItem(`contrast-${side}`)
+				);
+				console.log(getItem(`type-${side}`));
+				loadCoords(tcs.img, side);
+			}, refresh);
+		} else {
+			clearInterval(refreshTime);
+		}
+	});
 }
 
 const SIDES = document.querySelectorAll(".side");
 
 if (SIDES.length === 0) {
-  reload(getItem("type-main"));
+	reload(getItem("type-main"));
+	console.log(getItem("type-main"));
 }
 
 SIDES.forEach((side) => {
-  if (side.id === "leftSide") {
-    reload(getItem("type-leftSide"), side.id);
-  } else {
-    reload(getItem("type-rightSide"), side.id);
-  }
+	if (side.id === "leftSide") {
+		reload(getItem("type-leftSide"), side.id);
+	} else {
+		reload(getItem("type-rightSide"), side.id);
+	}
 });
